f25382ba37b99e1e284e87c9ddd8ee499d73df36
unknown <garciaalvarezgonzalo@gmail.com>
2023-05-02 17:05:07 +0200

    save

---
+++ js/main.js
@@ -0,0 +1,24 @@
+console.log('Superficies')
+
+//Cuadrado
+
+let cual = 2;              //Le damos valor a los lados del cuadrado
+
+
+//Triangulo
+
+let triangb = 4;
+                        //Y ahora le damos valores a la base y la altura del triangulo 
+let triangh = 5;
+
+//Area del cuadrado
+
+const acua = cual * cual;
+
+console.log ('El valor del area del cuadrado es '+ acua)
+
+//Area del triangulo
+
+const atriang = triangb * triangh / 2;
+
+console.log ('El valor del area del triangulo es '+ atriang)
